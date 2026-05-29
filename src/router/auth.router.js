2b231edd5ee322238345f6b386ec60155359bd6b
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-30 00:03:16 +0530

    Auth router configured

---
+++ src/router/auth.router.js
@@ -0,0 +1,7 @@
+// Importing modeules
+import express from "express";
+
+// Initializing the router
+const authRouter = express.Router();
+
+export default authRouter;
--- src/router/main.router.js
+++ src/router/main.router.js
@@ -1,7 +1,11 @@
 // Importing the modules
 import express from "express";
+import authRouter from "./auth.router.js";
 
 // Initialinzing the router
 const mainRouter = express.Router();
+
+// Configuring branch routers
+mainRouter.use("/auth", authRouter);
 
 export default mainRouter;
