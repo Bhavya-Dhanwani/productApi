f0c714ae72ac8b3a9ea23cf97ae8d8ebfbd92b9b
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-30 01:16:54 +0530

    Configured multer

---
+++ src/config/multer.config.js
@@ -0,0 +1,10 @@
+// Importing modules
+import multer from "multer";
+
+// Configuring memory storage
+const storage = multer.memoryStorage();
+
+// making the upload class to upload the images
+const upload = multer({ storage });
+
+export default upload;
