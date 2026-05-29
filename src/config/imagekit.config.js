892d62237ca210060d586e774b98092d42b37ee4
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-30 01:36:11 +0530

    Configured Imagekit

---
+++ src/config/imagekit.config.js
@@ -0,0 +1,9 @@
+import { ImageKit } from "@imagekit/nodejs/client.js";
+import envs from "./env.config.js";
+
+// Intializing the imagekit client
+const client = new ImageKit({
+    privateKey: envs.IMAGEKIT_PRIVATE_KEY, // Given the private key
+});
+
+export default ImageKit;
