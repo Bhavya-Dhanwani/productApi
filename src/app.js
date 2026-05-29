8e3206c2cf10c5e7cccb95cecd65134be2e4aaff
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-29 23:58:09 +0530

    Added the middlewares

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@
 import express from "express";
 import connectDB from "./config/db.config.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 // Initializing the app
 const app = express();
+
+// Adding the middlewares
+app.use(express.json());
+app.use(cookieParser());
 
 // Connecting to the DB
 await connectDB();
