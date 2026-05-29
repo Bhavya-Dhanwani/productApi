d059226f1aaab42627c20444ecde7a59061dd134
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-30 01:58:44 +0530

    Made the product Schema

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,10 @@
+// Importing modules
+import ApiError from "../utils/ApiError.util.js";
+
+/*
+@Route login
+@access public
+@use to Authenticate users
+@Type POST
+*/
+function createProduct(req, res) {}
---
+++ src/models/product.model.js
@@ -0,0 +1,23 @@
+// Importing module
+import mongoose from "mongoose";
+import { url } from "node:inspector";
+
+
+// Made a schema for the products
+const productSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    catageory: String,
+    images: [
+        {
+            url: String,
+            id: String
+        }
+    ]
+});
+
+
+// Made the model to handle the products in the database
+const productModel = await new mongoose.model("products", productSchema);
+export default productModel;
