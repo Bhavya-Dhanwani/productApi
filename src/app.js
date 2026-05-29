0c02063b0961b56c2cc1ee43d4b7398760d39e68
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-29 23:51:49 +0530

    Error middleware added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 // Importing Modules
 import express from "express";
 import connectDB from "./config/db.config.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 // Initializing the app
 const app = express();
 
 // Connecting to the DB
 await connectDB();
+
+app.use(errorMiddleware)
 
 export default app;
