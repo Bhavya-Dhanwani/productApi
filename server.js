c4a0ab62daa2d7d48b63ebbcdcfaa55a3528ea27
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-29 23:20:33 +0530

    Server setup completed

---
+++ server.js
@@ -0,0 +1,7 @@
+// Importing the app
+import app from "./src/app.js";
+
+// Starting the server
+app.listen(5000, () => {
+    console.log(`Server Is running on port 5000`);
+});
---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Importing Modules
+import express from "express";
+
+// Initializing the app
+const app = express();
+
+export default app;
