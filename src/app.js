dc158ce867e341ea363a1a8172fcafdede70a175
Bhavya-is-great <bdisop1234@gmail.com>
2026-05-30 11:44:19 +0530

    Made the ping endpoint to keep server awake

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.config.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
 import mainRouter from "./router/main.router.js";
+import ApiResponse from "./utils/ApiResponse.util.js";
 
 // Initializing the app
 const app = express();
@@ -17,6 +18,11 @@ await connectDB();
 
 // adding the /api router
 app.use("/api", mainRouter);
+
+// added a ping route to keep the server awake on render 
+app.get("/ping", (req, res) => {
+    return ApiResponse(res, 200, "Server is working");
+});
 
 app.use(errorMiddleware);
 
